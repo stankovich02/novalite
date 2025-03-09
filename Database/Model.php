99d7c17f853c05ff859c3cc0ce9abe0aec616ce5
Marko Stankovic <marefaca2002@gmail.com>
2025-03-09 17:50:45 +0100

    Added static where method

--- Database/Model.php
+++ Database/Model.php
@@ -154,12 +154,12 @@ abstract class Model
             $params = array_map(fn($item) => ":$item", array_keys($this->attributes));
             $sql = "INSERT INTO $this->table (".implode(',', array_keys($this->attributes)).") VALUES (".implode(',', $params).")";
         }
-        var_dump($this->attributes);
-        exit;
         $statement = self::$pdo->prepare($sql);
         foreach ($this->attributes as $attribute => $value) {
             $statement->bindValue(":$attribute", $value);
         }
+        var_dump($statement);
+        exit;
         return $statement->execute();
     }
 
